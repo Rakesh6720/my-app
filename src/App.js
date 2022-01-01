2dca06aa31d7401fc998b842b203ed965b428106
Rakesh Baruah <rbaruah@gmail.com>
2022-01-01 04:20:07 -0600

    initial-create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+const Header = () => {
+  const headerStyle = {
+    backgroundColor: "royalblue",
+    color: "lightyellow",
+    textAlign: "center",
+    fontFamily: "sans-serif",
+  };
+
+  return (
+    <div style={headerStyle}>
+      <h1>React-Three-Fiber</h1>
+    </div>
+  );
+};
+
+function MyAnimatedBox(props) {
+  const [active, setActive] = useState(false);
+  const myMesh = useRef();
+
+  function onClickHandler() {
+    setActive(!active);
+    if (!active) {
+      props.setMaterial("royalblue");
+    } else {
+      props.setMaterial("red");
+    }
+  }
+
+  useFrame(({ clock }) => {
+    myMesh.current.rotation.x = clock.getElapsedTime();
+    myMesh.current.rotation.y = clock.getElapsedTime();
+  });
+  return (
+    <mesh ref={myMesh} onClick={onClickHandler} scale={3}>
+      <boxGeometry />
+      <meshPhongMaterial color={props.color} />
+    </mesh>
+  );
+}
+
+const MyImage = () => {
+  const src = "./images/baptist.jpg";
+  return <img src={src} alt="me!" width="375" />;
+};
 
 function App() {
+  const containerStyle = {
+    backgroundColor: "lightyellow",
+    margin: 0,
+    padding: 0,
+  };
+  const [materialColor, setMaterialColor] = useState("royalblue");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-container" style={containerStyle}>
+      <Header />
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="yellow" position={[0, 0, 5]} />
+        <MyAnimatedBox color={materialColor} setMaterial={setMaterialColor} />
+      </Canvas>
+      <div>
+        {" "}
+        <Canvas>
+          <ambientLight intensity={(0, 1)} />
+          <directionalLight color="yellow" position={[0, 0, 5]} />
+          <MyAnimatedBox color={materialColor} setMaterial={setMaterialColor} />
+        </Canvas>
+      </div>
+      <div>
+        {" "}
+        <Canvas>
+          <ambientLight intensity={(0, 1)} />
+          <directionalLight color="yellow" position={[0, 0, 5]} />
+          <MyAnimatedBox color={materialColor} setMaterial={setMaterialColor} />
+        </Canvas>
+      </div>
+      <div>
+        {" "}
+        <Canvas>
+          <ambientLight intensity={(0, 1)} />
+          <directionalLight color="yellow" position={[0, 0, 5]} />
+          <MyAnimatedBox color={materialColor} setMaterial={setMaterialColor} />
+        </Canvas>
+      </div>
     </div>
   );
 }
